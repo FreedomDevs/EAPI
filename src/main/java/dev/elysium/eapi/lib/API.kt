3f56f46845e7e2f95383ab811b29a50604c3a322
foksik <107531680+Foksif@users.noreply.github.com>
2025-04-20 00:40:53 +0700

    Добавлен эндпоинт для проверки токена

--- src/main/java/dev/elysium/eapi/lib/API.kt
+++ src/main/java/dev/elysium/eapi/lib/API.kt
@@ -1,5 +1,6 @@
 package dev.elysium.eapi.lib
 
+import dev.elysium.eapi.lib.endpoints.CheckTokenValid
 import dev.elysium.eapi.lib.endpoints.GetHealth
 import dev.elysium.eapi.lib.endpoints.GetUser
 
@@ -7,9 +8,11 @@ class API(internal val baseUrl: String, internal val token: String) {
 
   val getHealth = GetHealth
   val getUser = GetUser
+  val checkTokenValid = CheckTokenValid
 
   init {
     getHealth.inject(this)
     getUser.inject(this)
+    checkTokenValid.inject(this)
   }
 }
